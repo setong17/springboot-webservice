925f65fb9262391fdfd48a21dd66d06778cadfbf
syson <setong17@gmail.com>
2021-09-20 16:51:06 +0900

    mustache

--- src/main/java/com/setong/test/springboot/web/IndexController.java
+++ src/main/java/com/setong/test/springboot/web/IndexController.java
@@ -1,14 +1,43 @@
 package com.setong.test.springboot.web;
 
+import com.setong.test.springboot.service.PostsService;
+import com.setong.test.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+//        model.addAttribute("posts", postsService.findById(id));
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+
+        return "posts-update";
+    }
+
+
 
 }
