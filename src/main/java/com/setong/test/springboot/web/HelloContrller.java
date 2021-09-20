e01b7c9dbaa88654d682a791e6cbae5a7e6a99d8
syson <setong17@gmail.com>
2021-09-20 11:11:49 +0900

    p.135 까지

---
+++ src/main/java/com/setong/test/springboot/web/HelloContrller.java
@@ -0,0 +1,22 @@
+package com.setong.test.springboot.web;
+
+import com.setong.test.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContrller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+}
